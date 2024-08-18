487fc0689b201a41721f8bb13a2c373477b75e5a
binking338 <binking338@163.com>
2024-08-18 23:25:14 +0800

    持久化监听增加getBean方法

--- ddd-domain-repo-jpa/src/main/java/org/netcorepal/cap4j/ddd/domain/repo/AbstractJpaPersistListener.java
+++ ddd-domain-repo-jpa/src/main/java/org/netcorepal/cap4j/ddd/domain/repo/AbstractJpaPersistListener.java
@@ -1,5 +1,8 @@
 package org.netcorepal.cap4j.ddd.domain.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
 /**
  * 基于Jpq的实体持久化监听抽象类
  *
@@ -7,6 +10,19 @@ package org.netcorepal.cap4j.ddd.domain.repo;
  * @date 2024/3/9
  */
 public abstract class AbstractJpaPersistListener<Entity> implements PersistListener<Entity> {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    ApplicationContext applicationContext;
+
+    public <T> T getBean(Class<T> clazz){
+        return applicationContext.getBean(clazz);
+    }
+    public <T> T getBean(String name,  Class<T> clazz){
+        return applicationContext.getBean(name, clazz);
+    }
+
+
     public abstract Class<Entity> forEntityClass();
 
     public boolean throwOnException() {
