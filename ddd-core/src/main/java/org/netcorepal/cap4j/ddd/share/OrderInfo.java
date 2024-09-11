4c1f1856c8be02f512fdec7bfcdefad3a98ed233
binking338 <binking338@163.com>
2024-09-11 19:16:13 +0800

    分页排序增加fluent风格方法

--- ddd-core/src/main/java/org/netcorepal/cap4j/ddd/share/OrderInfo.java
+++ ddd-core/src/main/java/org/netcorepal/cap4j/ddd/share/OrderInfo.java
@@ -18,13 +18,39 @@ public class OrderInfo {
     /**
      * 排序字段
      */
-    @Schema(description="排序字段")
+    @Schema(description = "排序字段")
     @ApiModelProperty(value = "排序字段")
     String field;
     /**
      * 是否降序
      */
-    @Schema(description="是否降序")
+    @Schema(description = "是否降序")
     @ApiModelProperty(value = "是否降序")
     Boolean desc;
+
+    /**
+     * 降序
+     *
+     * @param field
+     * @return
+     */
+    public static OrderInfo desc(String field) {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.field = field;
+        orderInfo.desc = true;
+        return orderInfo;
+    }
+
+    /**
+     * 升序
+     *
+     * @param field
+     * @return
+     */
+    public static OrderInfo asc(String field) {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.field = field;
+        orderInfo.desc = false;
+        return orderInfo;
+    }
 }
