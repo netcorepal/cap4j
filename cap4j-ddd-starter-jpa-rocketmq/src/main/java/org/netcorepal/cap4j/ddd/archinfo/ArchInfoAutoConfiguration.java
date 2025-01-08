93193f404442b5e5466ca8baf3ff057ca060fba6
binking338 <binking338@163.com>
2025-01-08 16:55:52 +0800

    架构信息优化

--- cap4j-ddd-starter-jpa-rocketmq/src/main/java/org/netcorepal/cap4j/ddd/archinfo/ArchInfoAutoConfiguration.java
+++ cap4j-ddd-starter-jpa-rocketmq/src/main/java/org/netcorepal/cap4j/ddd/archinfo/ArchInfoAutoConfiguration.java
@@ -1,9 +1,11 @@
 package org.netcorepal.cap4j.ddd.archinfo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.netcorepal.cap4j.ddd.archinfo.configure.ArchInfoProperties;
 import org.netcorepal.cap4j.ddd.archinfo.web.ArchInfoRequestHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,7 @@ import static org.netcorepal.cap4j.ddd.share.Constants.CONFIG_KEY_4_SVC_VERSION;
  * @date 2024/11/24
  */
 @Configuration
+@Slf4j
 public class ArchInfoAutoConfiguration {
 
     @Bean
@@ -31,9 +34,16 @@ public class ArchInfoAutoConfiguration {
     }
 
 
+    @Value("${server.port:80}")
+    private String serverPort;
+    @Value("${server.servlet.context-path:/}")
+    private String serverServletContentPath;
+
+    @ConditionalOnWebApplication
     @ConditionalOnProperty(name = "cap4j.ddd.archinfo.enabled", havingValue = "true")
     @Bean(name = "/cap4j/archinfo")
     public ArchInfoRequestHandler archInfoRequestHandler(ArchInfoManager archInfoManager) {
+        log.info("archinfo URL: http://localhost:" + serverPort + serverServletContentPath + "/cap4j/archinfo");
         return new ArchInfoRequestHandler(archInfoManager);
     }
 }
