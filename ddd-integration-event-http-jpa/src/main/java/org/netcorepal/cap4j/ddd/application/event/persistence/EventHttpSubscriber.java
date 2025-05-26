92e33008844e8c6f7ed0099d2d73a7cc909b0011
binking338 <binking338@163.com>
2025-05-26 14:56:55 +0800

    集成事件HTTP订阅支持单独重试

--- ddd-integration-event-http-jpa/src/main/java/org/netcorepal/cap4j/ddd/application/event/persistence/EventHttpSubscriber.java
+++ ddd-integration-event-http-jpa/src/main/java/org/netcorepal/cap4j/ddd/application/event/persistence/EventHttpSubscriber.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.netcorepal.cap4j.ddd.domain.aggregate.annotation.Aggregate;
 
 /**
@@ -21,6 +23,8 @@ import org.netcorepal.cap4j.ddd.domain.aggregate.annotation.Aggregate;
 @Table(name = "`__event_http_subscriber`")
 @DynamicInsert
 @DynamicUpdate
+@SQLDelete(sql = "update `__event_http_subscriber` set `db_deleted` = 1 where `id` = ? and `version` = ? ")
+@Where(clause = "`db_deleted` = 0")
 
 @AllArgsConstructor
 @NoArgsConstructor
